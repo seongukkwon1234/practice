f3faf3d8f3012d84a1e5da7c1203d2aaa26ed68f
seongukkwon1234 <seongukkwon38@gmail.com>
2024-07-01 01:07:05 +0900

    6/30_Cword practice

---
+++ printf,scanf.C
@@ -0,0 +1,59 @@
+/*#include <stdio.h>
+
+int main(void)
+{
+	//정수형 변수에 대한 예제
+	/*int age = 12;
+	printf("%d\n", age);
+	age = 13;
+	printf("%d\n", age);*/
+
+	//printf("1\n");
+	//printf("2\n");     //2는 범위에서 벗어남으로 주석 처리
+	//printf("3\n");
+
+	//실수형 변수에 대한 예제
+
+	/*float f = 46.5;
+	printf("%.2f\n", f);
+	double d=4.428;
+	printf("%.2lf\n", d);*/
+
+	//const int Year = 2000;      //상수 
+	//printf("태어난 년도 : %d\n", Year);
+	//Year = 2001
+
+	//printf
+	//연산
+	//int add = 3 + 7;   //10
+	//printf("3+7= %d\n", add);
+	//printf("%d x %d = %d\n", 30, 79, 30 * 79);
+
+	//scanf 
+	// 키보드 입력을 받아서 저장
+	/*int input;
+	printf("값을 입력하세요:");
+	scanf_s("%d", &input);
+	printf("입력값 : %d\n", input);*/
+
+	/*int one, two, three;
+	printf("3개의 정수를 입력하세요 : ");
+	scanf_s("%d %d %d", &one, &two, &three);
+	printf("첫번째 값 : %d\n", one);
+	printf("두번째 값 : %d\n", two);
+	printf("세번째 값 : %d\n", three);*/
+
+	//문자, 문자열
+	/*char c = 'A';
+	printf("%c\n", c);*/
+
+	/*char str[256];
+	scanf_s("%s", str, sizeof(str));
+	printf("%s\n", str);
+
+	
+	
+
+
+	return 0;
+}*/
