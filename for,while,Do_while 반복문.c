01355dabd62661beb2006b22640b904d63989e80
seongukkwon1234 <seongukkwon38@gmail.com>
2024-07-01 23:43:13 +0900

    for,while,do_while_07_01

---
+++ for,while,Do_while 반복문.c
@@ -0,0 +1,93 @@
+/*#include <stdio.h>
+
+int main(void)
+{
+	//++
+	/*int a = 10;
+	printf("a는 %d\n" , a);
+	a ++ ;
+	printf("a는 %d\n", a);*/
+
+	//int b = 20;
+	//b = b + 1;
+	//printf("b는 %d\n", ++b);      //b + 1 = 21
+	//printf("b는 %d\n", b++);      //21 인 상태에서 다음 에 ++을 해라
+	//printf("b는 %d\n", b);
+
+	//int i = 1;
+	//printf("Hello World %d\n", i++);
+
+	//반복문
+	//for, whlie, do while
+
+	//for(선언; 조건; 증감)
+	//for (int i = 1; i <= 10; i++)
+	//{
+	//	printf("Hello World %d\n", i);
+	//}
+
+	//whlie (조건) {  }
+	/*int i = 1;
+	while (i <= 10)
+	{
+		printf("Hello World %d\n", i++);
+	}*/
+
+	//do {  } while(조건);
+	/*int i = 1;                     //선언
+	do {                              
+		printf("Hello World %d\n", i);
+		i++;                     //증감
+	} while (i <= 10);    //조건*/
+
+	//2중 반복문
+	/*for (int i = 1; i <= 3; i++)
+	{
+		printf("첫 번째 반복문 :%d\n", i);
+
+		for (int j = 1; j <= 5; j++)
+		{
+			printf("    두 번째 반복문 :%d\n", j);
+		}                                           
+	}*/
+
+	//구구단
+	/*for (int i = 2; i <= 9; i++)           //몇단 까지 할건지 지정
+	{
+		printf("%d단 계산\n", i);
+		for(int j = 1; j <=9; j ++)             //%d단 계산
+		{
+			printf("   %d x %d = %d\n", i, j, i * j);
+		}
+	}*/
+
+	//피라미드
+    /*for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	//반대로 피라미드
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i; j < 4; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k <= i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
+
+	
+
+	return 0;
+}*/
